b8c201f6f4200c3d7950cb7bd2695fb234c4b54d
mv-31 <96203101+mv-31@users.noreply.github.com>
2022-08-23 12:25:46 +0300

    v0.2.1 23.08.22 Foodgram

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -32,10 +32,14 @@ class RecipeFilter(filters.FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
 
     def favorite_filter(self, **kwargs):
-        return Recipe.objects.filter(favorite_recipe__user=self.request.user)
+        return Recipe.objects.filter(
+            favorite_recipe__user=self.request.user
+        )
 
     def shopping_cart_filter(self, **kwargs):
-        return Recipe.objects.filter(shopping_recipe__user=self.request.user)
+        return Recipe.objects.filter(
+            shopping_recipe__user=self.request.user
+        )
 
     class Meta:
         model = Recipe
